7838dc38d5323e4bb7e6230e2216710c7a9561e1
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-09-25 12:33:37 +0530

    add middleware

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const port = 3001;
 const stripeClient = stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
-app.use(express.json());
+
+app.use((req, res, next) => {
+  if (req.originalUrl === "/webhook") {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
 
 const plans = {
   individual: {
